cd835550c8e465e4163c84abfdc6d89592dc1832
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-05-16 22:03:43 +0300

    Fix project

--- public/napopravky/js/scripts.js
+++ public/napopravky/js/scripts.js
@@ -1,88 +1,229 @@
-'use strict';
+let features = document.querySelectorAll('.js-slider__list-item');
+let controls = document.querySelectorAll('.js-controls__button');
 
-const MODAL = document.querySelector('.js-modal');
-const HEADER = document.querySelector('.js-modal__header');
-const BODY = document.querySelector('body');
-const DIALOG = document.querySelector('.js-modal__section');
-const OPEN_BUTTON = document.querySelector('.js-user-info__button');
-const CLOSE_BUTTON = document.querySelector('.js-modal__close-button');
-const TMPL = document.querySelector('#template');
-const CHRONIC_LIST = document.querySelector('.js-form__chronic-list');
-const CHRONIC_INPUT = document.querySelector('.js-form__chronic-input');
-const CHRONIC_ITEM = TMPL.content.querySelector('.form__chronic-item');
+if ( document.documentElement.clientWidth >= 1200 ) {
+  features[0].classList.add('slider__list-item--active');
+  features[1].classList.add('slider__list-item--active');
+  features[2].classList.remove('slider__list-item--active');
+  features[3].classList.remove('slider__list-item--active');
 
-OPEN_BUTTON.addEventListener('click', open);
+  features[0].classList.remove('slider__list-item--visually-hidden');
+  features[1].classList.remove('slider__list-item--visually-hidden');
+  features[2].classList.add('slider__list-item--visually-hidden');
+  features[3].classList.add('slider__list-item--visually-hidden');
 
-CLOSE_BUTTON.addEventListener('click', close);
+  controls[0].classList.add('controls__button--choosed');
+  controls[1].classList.add('controls__button--hidden');
+  controls[3].classList.add('controls__button--hidden');
+}
 
-MODAL.addEventListener('click', function(event){
-  let target = event.target;
-  console.log(target);
+controls[0].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
   
-  if ( target.closest('.chronic-disease__button') ) {
-    removeElem(event);
-  } else if ( !DIALOG.contains(target) ) {
-    close();
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('slider__list-item--visually-hidden');
+    features[1].classList.add('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
   }
-});
 
-BODY.addEventListener('keydown', function(event){
-  let keyCode = event.key;
-  if (keyCode === 'Escape' && MODAL.classList.contains('modal_active')) {
-    close();
-  }  
-});
+  if ( document.documentElement.clientWidth >= 1200 ) {
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
 
-CHRONIC_INPUT.addEventListener('keydown', function(event) {
-  let keyCode = event.key;
-  let textInput;
-  if (keyCode === 'Enter') {
-    event.preventDefault();
-    textInput = CHRONIC_INPUT.value;
-    createElement(textInput);
-  }
-});
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
 
-
-
-function open() {
-  BODY.classList.add('modal-active');
-  MODAL.classList.add('modal_active');
-  OPEN_BUTTON.setAttribute('aria-hidden', false);
-
-  let headerHeight = HEADER.offsetHeight;
-  if (document.documentElement.clientWidth < 768) {
-    DIALOG.setAttribute('style', `margin-top: ${headerHeight}px;`);
+    features[0].classList.remove('slider__list-item--visually-hidden');
+    features[1].classList.remove('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
   }
 }
 
-function close() {
-  BODY.classList.remove('modal-active');
-  MODAL.classList.remove('modal_active');
-  OPEN_BUTTON.setAttribute('aria-hidden', true);
+controls[1].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
 
-  if (document.documentElement.clientWidth < 768) {
-    DIALOG.removeAttribute('style');
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.add('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('slider__list-item--visually-hidden');
+    features[1].classList.remove('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
   }
 }
 
-function removeElem(event) {
-  let elem = event.target.closest('.form__chronic-item');
-  elem.remove();
+controls[2].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.add('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.add('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.add('slider__list-item--visually-hidden');
+    features[1].classList.add('slider__list-item--visually-hidden');
+    features[2].classList.remove('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
+  }
+
+  if ( document.documentElement.clientWidth >= 1200 ) {
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.add('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.add('slider__list-item--active');
+    features[3].classList.add('slider__list-item--active');
+
+    features[0].classList.add('slider__list-item--visually-hidden');
+    features[1].classList.add('slider__list-item--visually-hidden');
+    features[2].classList.remove('slider__list-item--visually-hidden');
+    features[3].classList.remove('slider__list-item--visually-hidden');
+  }
 }
 
-function createElement(text) {
-  if (CHRONIC_INPUT.value != '') {
-    let newElement = CHRONIC_ITEM.cloneNode(true);
-    CHRONIC_LIST.appendChild(newElement);
+controls[3].onclick = function() {
+  if ( document.documentElement.clientWidth < 1200 ) {
 
-    let newElementInput = newElement.querySelector('.chronic-disease__input');
-    let newElementText = newElement.querySelector('.chronic-disease__text');
-    let newElementButton = newElement.querySelector('.chronic-disease__button');
-
-    newElementInput.setAttribute('value', text);
-    newElementText.textContent = text;
-    newElementButton.setAttribute('aria-label', `Удалить заболевание ${text}`)
+    controls[0].classList.remove('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.add('controls__button--choosed');
+  
+    features[0].classList.remove('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.add('slider__list-item--active');
+  
+    features[0].classList.add('slider__list-item--visually-hidden');
+    features[1].classList.add('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.remove('slider__list-item--visually-hidden');
   }
-  CHRONIC_INPUT.value = '';
+}
+
+window.addEventListener('resize', function(event) {
+  if ( document.documentElement.clientWidth >= 1200 ) {
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.add('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('slider__list-item--visually-hidden');
+    features[1].classList.remove('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
+  
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.add('controls__button--hidden');
+    controls[3].classList.add('controls__button--hidden');
+  } 
+    else if ( document.documentElement.clientWidth < 1200 ) {
+
+    controls[0].classList.add('controls__button--choosed');
+    controls[1].classList.remove('controls__button--choosed');
+    controls[2].classList.remove('controls__button--choosed');
+    controls[3].classList.remove('controls__button--choosed');
+
+    controls[1].classList.remove('controls__button--hidden');
+    controls[3].classList.remove('controls__button--hidden');
+  
+    features[0].classList.add('slider__list-item--active');
+    features[1].classList.remove('slider__list-item--active');
+    features[2].classList.remove('slider__list-item--active');
+    features[3].classList.remove('slider__list-item--active');
+  
+    features[0].classList.remove('slider__list-item--visually-hidden');
+    features[1].classList.add('slider__list-item--visually-hidden');
+    features[2].classList.add('slider__list-item--visually-hidden');
+    features[3].classList.add('slider__list-item--visually-hidden');
+  }
+});
+
+let headerMenu = document.querySelector('.js-header-menu');
+let headerSubMenuList = document.querySelector('.js-header-menu__submenu-list');
+let headerSubMenuButton = document.querySelector('.js-header-menu__submenu');
+let buttonOpen = document.querySelector('.js-menu-button--open');
+let buttonClose = document.querySelector('.js-menu-button--close');
+let windowsWidht = document.documentElement.clientWidth;
+
+window.addEventListener('resize', function(event) {
+  windowsWidht = document.documentElement.clientWidth;
+});
+
+buttonOpen.onclick = function() {
+  if ( windowsWidht < 1024 ) {
+    headerMenu.classList.add('header-menu--open');
+    buttonOpen.classList.add('header__menu-button--hidden');
+    buttonClose.classList.remove('header__menu-button--hidden');
+    headerMenu.setAttribute('aria-expanded', true);
+  }
+}
+
+buttonClose.onclick = function() {
+  if ( windowsWidht < 1024 ) {
+    headerMenu.classList.remove('header-menu--open');
+    buttonOpen.classList.remove('header__menu-button--hidden');
+    buttonClose.classList.add('header__menu-button--hidden');
+    headerMenu.setAttribute('aria-expanded', false);
+  }
+}
+
+  document.body.onclick = function (e) {
+    if ( windowsWidht < 1024 ) {
+      e = e || event;
+      target = e.target || e.srcElement;
+    
+      if ( (headerMenu.getAttribute('aria-expanded') == 'true') && !(headerMenu.contains(target)) && !(buttonOpen.contains(target)) && !(buttonClose.contains(target)) ) {
+    
+        headerMenu.classList.remove('header-menu--open');
+        buttonOpen.classList.remove('header__menu-button--hidden');
+        buttonClose.classList.add('header__menu-button--hidden');
+        headerMenu.setAttribute('aria-expanded', false);
+      }
+  }
+}
+
+headerSubMenuButton.onclick = function() {
+  headerSubMenuButton.classList.toggle('header-menu__submenu--open');
+  headerSubMenuList.classList.toggle('header-menu__submenu-list--open');
+
+  if (headerSubMenuList.getAttribute('aria-expanded') == 'true') {
+    headerSubMenuList.setAttribute('aria-expanded', false);
+  } else {
+    headerSubMenuList.setAttribute('aria-expanded', true);
+  }
 }
